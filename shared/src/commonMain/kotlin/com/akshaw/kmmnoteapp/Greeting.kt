5a50ef638cc1acc8e55284c1c14bd4086ef8411a
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-04-18 18:46:40 +0530

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/akshaw/kmmnoteapp/Greeting.kt
@@ -0,0 +1,9 @@
+package com.akshaw.kmmnoteapp
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/akshaw/kmmnoteapp/Platform.kt
@@ -0,0 +1,7 @@
+package com.akshaw.kmmnoteapp
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
